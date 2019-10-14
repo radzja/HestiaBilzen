8bdf5ceb5b49f18a459789ef2168198d5289ac04
Stephen Hermans <hermans.stephen@gmail.com>
2019-10-14 13:39:52 +0200

    Added calender component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactformComponent } from './contactform/contactform.component';
+import { CalenderComponent } from './calender/calender.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactformComponent
+    ContactformComponent,
+    CalenderComponent
   ],
   imports: [
     BrowserModule,
