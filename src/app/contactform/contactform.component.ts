521ef1152558d1012bba605c4e31db11e90a7348
Frank <frank.martens10@gmail.com>
2019-10-21 10:25:20 +0200

    Responsive contact formulier met validatie

--- src/app/contactform/contactform.component.ts
+++ src/app/contactform/contactform.component.ts
@@ -10,49 +10,22 @@ import { Observable } from 'rxjs';
 })
   
   export class ContactFormComponent implements OnInit {
-  ngOnInit(): void {
-    throw new Error("Method not implemented.");
-  }
+ 
+    subject: Array<String> = ['Inschrijving', 'Opmerking', 'Andere'];
+    user = { 'fname': 'Frank', 'lname' : 'Martens'};
+    
+    emailPattern = "^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$";
 
-    originalContactSettings : ContactSettings = {
-      name: null,
-      emailOffers: null,
-      interfaceStyle: null,
-      subscriptionType: null,
-      notes: null
-    };
-  
-    userRating = 0;
-    maxRating = 10;
-    contactSettings : ContactSettings = { ...this.originalContactSettings };
-    postError = false;
-    postErrorMessage = '';
-    subscriptionTypes: Observable<string[]>;
-  
-    onBlur(field : NgModel) {
-      console.log('in onBlur: ', field.valid);
+    constructor() { }
+
+
+    ngOnInit(){
     }
-  
-    onHttpError(errorResponse: any) {
-      console.log('error: ', errorResponse);
-      this.postError = true;
-      this.postErrorMessage = errorResponse.error.errorMessage;
-    }
-  
-    onSubmit(form: NgForm) {
-      console.log('in onSubmit: ', form.value);
-  
-      // if (form.valid) {
-      //   this.dataService.postUserSettingsForm(this.userSettings).subscribe(
-      //     result => console.log('success: ', result),
-      //     error => this.onHttpError(error)
-      //   );
-      // }
-      // else {
-      //   this.postError = true;
-      //   this.postErrorMessage = "Please fix the above errors"
-      // }
-    }
-  
+
+    submitHandler(myForm: any) {
+      // console.log(myForm);
+      console.log('Model Value', this.user);
+      console.log('Form Value', myForm.value);
+    }  
   }
   
