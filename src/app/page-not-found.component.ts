a77e00793418adfe463ff99789d8a7452614b2a8
Frank <frank.martens10@gmail.com>
2019-10-23 10:11:42 +0200

    Page not found -> 404 gemaakt

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: `
-    <h1>This is not the page you were looking for!</h1>
-    `
+  templateUrl: './page-not-found.component.html',
+  styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent { }
