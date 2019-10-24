cf3376fbe88df95a5e074335c7cc4ba9af973867
Frank <frank.martens10@gmail.com>
2019-10-24 10:29:52 +0200

    Op de documentenpagina een upload button voorzien, moet nog gekoppeld worden met de backend

--- src/app/documenten/documenten.component.ts
+++ src/app/documenten/documenten.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from  '@angular/forms';
+import { UploadService } from  '../upload.service';
 
 @Component({
   selector: 'app-documenten',
@@ -7,10 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DocumentenComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+  error: string;
+  userId: number = 1; /* gekoppeld aan de userdid in de tabel als die er is,anders aanpassen*/
+  
+  uploadResponse = { status: '', message: '', filePath: '' };
+
+ 
+
+  constructor(private formBuilder: FormBuilder, private uploadService: UploadService) { }
 
   ngOnInit() {
+    this.form = this.formBuilder.group({
+      docs: ['']
+    });
   }
 
+
+  onFileChange(event) {
+    if (event.target.files.length > 0) {
+      const file = event.target.files[0];
+      this.form.get('docs').setValue(file);
+    }
+  }
+
+  onSubmit() {
+    const formData = new FormData();
+    formData.append('file', this.form.get('docs').value);
+
+    this.uploadService.upload(formData, this.userId).subscribe(
+      (res) => this.uploadResponse = res,
+      (err) => this.error = err
+    );
+  }
   
 }
