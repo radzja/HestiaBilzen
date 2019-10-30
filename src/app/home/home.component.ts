e07e8d5146898fdcf5cd6dd3fa0f42deeb6b100f
Stephen Hermans <hermans.stephen@gmail.com>
2019-10-30 12:22:38 +0100

    Implementing interface backend

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { IWedstrijden } from '../wedstrijden';
 
 @Component({
- 
+
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
@@ -10,25 +10,11 @@ export class HomeComponent implements OnInit {
 
   hideLogo  : boolean = true;
   showGmaps : boolean = false;
-<<<<<<< HEAD
-
-  wedstrijden: IWedstrijden[] = [
-    {"code":"QM104","matchDate":"26-10-19","matchTime":"14:30","homeTeam":"HC MAASMECHELEN","awayTeam":"U13-M","out":1,"sportsHallId":17},
-    {"code":"10062","matchDate":"26-10-19","matchTime":"17:00","homeTeam":"U19-M","awayTeam":"MARGRATEN","out":0,"sportsHallId":1},
-    {"code":"QO79","matchDate":"02-11-19","matchTime":"17:00","homeTeam":"U12","awayTeam":"Hannibal Tessenderlo","out":0,"sportsHallId":1},
-    {"code":"10063","matchDate":"02-11-19","matchTime":"15:15","homeTeam":"VIOS","awayTeam":"U19-M","out":1,"sportsHallId":1}
-    ];
-
-
-
-
-=======
->>>>>>> aed3abef6e6356c23f9b53fd42d31c6a303046ab
   constructor() { }
 
   ngOnInit() {
-  }  
-  
+  }
+
 toggleGmaps (): void {
   this.hideLogo = !this.hideLogo;
   this.showGmaps = !this.showGmaps;
--- src/app/kalender/kalender.component.ts
+++ src/app/kalender/kalender.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IWedstrijden } from '../wedstrijden';
 
 @Component({
   selector: 'app-kalender',
@@ -6,13 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./kalender.component.css']
 })
 export class KalenderComponent implements OnInit {
-  pageTitle: string = 'Competitie kalender 2019 - 2020';
+  pageTitle = 'Competitie kalender 2019 - 2020';
 
-  wedstrijden: any [] = [
-  {"code":"QM104","matchDate":"26-10-19","matchTime":"14:30","homeTeam":"HC MAASMECHELEN","awayTeam":"U13-M","out":"1","sportsHallId":"17"},
-  {"code":"10062","matchDate":"26-10-19","matchTime":"17:00","homeTeam":"U19-M","awayTeam":"MARGRATEN","out":"0","sportsHallId":"1"},
-  {"code":"QO79","matchDate":"02-11-19","matchTime":"17:00","homeTeam":"U12","awayTeam":"Hannibal Tessenderlo","out":"0","sportsHallId":"1"},
-  {"code":"10063","matchDate":"02-11-19","matchTime":"15:15","homeTeam":"VIOS","awayTeam":"U19-M","out":"1","sportsHallId":"1"}
+  wedstrijden: IWedstrijden [] = [
+  {code: 'QM104', matchDate: '26-10-19', matchTime: '14:30', homeTeam: 'HC MAASMECHELEN', awayTeam: 'U13-M', out: 1, sportsHallId: 17},
+  {code: '10062', matchDate: '26-10-19', matchTime: '17:00', homeTeam: 'U19-M', awayTeam: 'MARGRATEN', out: 0, sportsHallId: 1},
+  {code: 'QO79', matchDate: '02-11-19', matchTime: '17:00', homeTeam: 'U12', awayTeam: 'Hannibal Tessenderlo', out: 0, sportsHallId: 1},
+  {code: '10063', matchDate: '02-11-19', matchTime: '15:15', homeTeam: 'VIOS', awayTeam: 'U19-M', out: 1, sportsHallId: 1}
   ];
 
 
