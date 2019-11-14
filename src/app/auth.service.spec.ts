5db26ab5488f786e0dd432836f7ea8a7958b4a0d
Stephen Hermans <hermans.stephen@gmail.com>
2019-11-14 10:58:54 +0100

    Disabled all auto generated test classes & added first test for AuthService

--- src/app/auth.service.spec.ts
+++ src/app/auth.service.spec.ts
@@ -1,12 +1,60 @@
-import { TestBed } from '@angular/core/testing';
-
+// import { TestBed } from '@angular/core/testing';
 import { AuthService } from './auth.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 describe('AuthService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AuthService;
+  beforeEach(() => {
+    service = new AuthService();
+    // TestBed.configureTestingModule();
+    // this.service
+    service.logout();
+  }
+  ); // TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: AuthService = TestBed.get(AuthService);
-    expect(service).toBeTruthy();
+  it('isLoggedIn should be false to start with', () => {
+    // const service: AuthService = new AuthService();
+    // Act
+    expect(service.isLoggedIn()).toBeFalsy();
   });
+
+  /* it('isLoggedIn should be true after successful authentication', () => {
+    // Arrange
+    let formBuilder: FormBuilder;
+    const loginForm: FormGroup = formBuilder.group({
+      email: ['hermans.stephen@gmail.com'],
+      password: ['123456789']
+    });
+    this.service.login(loginForm);
+
+    // Act
+    expect(this.service.isLoggedIn()).toBeTruthy();
+  });
+
+  it('login & isLoggedIn should return true after successful authentication', () => {
+    // Arrange
+    let formBuilder: FormBuilder;
+    const loginForm: FormGroup = formBuilder.group({
+      email: ['hermans.stephen@gmail.com'],
+      password: ['123456789']
+    });
+    this.service.login(this.loginForm);
+
+    // Act
+    expect(this.service.login(this.loginForm)).toBeTruthy();
+    expect(this.service.isLoggedIn()).toBeTruthy();
+  });
+
+  it('login & isLoggedIn should return false after authentication with incorrect credentials', () => {
+    // Arrange
+    let formBuilder: FormBuilder;
+    const loginForm: FormGroup = formBuilder.group({
+      email: ['unauthorized@user.be'],
+      password: ['123456789']
+    });
+
+    // Act
+    expect(this.service.login(this.loginForm)).toBeFalsy();
+    expect(this.service.isLoggedIn()).toBeFalsy();
+  }); */
 });
